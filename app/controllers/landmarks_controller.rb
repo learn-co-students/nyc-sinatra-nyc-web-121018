bcf8c5ec24c95332de1fd907b11d47451fc9e281
Nison Malayev <nmalayev@gmail.com>
2019-01-10 10:24:39 -0500

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -8,4 +8,29 @@ class LandmarksController < ApplicationController
     erb :'landmarks/new'
   end
 
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  post '/landmarks' do
+    # binding.pry
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark])
+    end
+
+    redirect :"/landmarks/#{@landmark.id}"
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+    
+    redirect :"/landmarks/#{@landmark.id}"
+  end
 end
